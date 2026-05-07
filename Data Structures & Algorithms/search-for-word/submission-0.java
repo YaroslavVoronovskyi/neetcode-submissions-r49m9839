34228fbf5ecdce90ea61e143f039ad4f01359572
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-05-07 14:03:10 +0300

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    private int ROWS;
+    private int COLS;
+    private Set<Pair<Integer, Integer>> path = new HashSet<>();
+
+    public boolean exist(char[][] board, String word) {
+        ROWS = board.length;
+        COLS = board[0].length;
+
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (depthFiratSearch(board, word, r, c, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean depthFiratSearch(char[][] board, String word, int r, int c, int i) {
+        if (i == word.length()) {
+            return true;
+        }
+        if (r < 0 || c <0 || r >= ROWS || c >= COLS || board[r][c] != word.charAt(i) || path.contains(new Pair<>(r, c))) {
+            return false;
+        }
+        path.add(new Pair<>(r, c));
+        boolean result = depthFiratSearch(board, word, r + 1, c, i + 1) ||
+                         depthFiratSearch(board, word, r - 1, c, i + 1) ||
+                         depthFiratSearch(board, word, r, c + 1, i + 1) ||
+                         depthFiratSearch(board, word, r, c - 1, i + 1);
+        path.remove(new Pair<>(r, c));
+        return result;
+    }
+}
