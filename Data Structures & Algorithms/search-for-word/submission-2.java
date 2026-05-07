3db79590e35d157dbcda2c992c8e7541b52f951a
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-05-07 14:37:20 +0300

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    private int ROWS;
+    private int COLS;
+    private boolean[][] isVisited;
+
+    public boolean exist(char[][] board, String word) {
+        ROWS = board.length;
+        COLS = board[0].length;
+        isVisited = new boolean[ROWS][COLS];
+
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (depthFirstSearch(board, word, r, c, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean depthFirstSearch(char[][] board, String word, int r, int c, int i) {
+        if (i == word.length()) {
+            return true;
+        }
+        if (r < 0 || c <0 || r >= ROWS || c >= COLS || 
+        board[r][c] != word.charAt(i) || isVisited[r][c]) {
+            return false;
+        }
+        isVisited[r][c] = true;
+        boolean result = depthFirstSearch(board, word, r + 1, c, i + 1) ||
+                         depthFirstSearch(board, word, r - 1, c, i + 1) ||
+                         depthFirstSearch(board, word, r, c + 1, i + 1) ||
+                         depthFirstSearch(board, word, r, c - 1, i + 1);
+        isVisited[r][c] = false;
+        return result;
+    }
+}
