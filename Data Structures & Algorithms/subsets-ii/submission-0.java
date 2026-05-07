f0f606a456b2ea04bac7d9c2189a8b4b1305ac95
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-05-07 10:06:31 +0300

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    private Set<List<Integer>> result = new HashSet<>();
+
+    public List<List<Integer>> subsetsWithDup(int[] numbers) {
+        Arrays.sort(numbers);
+        backtrack(numbers, 0, new ArrayList<>());
+        return new ArrayList<>(result);
+    }
+
+    private void backtrack(int[] numbers, int index, List<Integer> subset) {
+        if (index == numbers.length) {
+            result.add(new ArrayList<>(subset));
+            return;
+        }
+        subset.add(numbers[index]);
+        backtrack(numbers, index + 1, subset);
+        subset.remove(subset.size() - 1);
+        backtrack(numbers, index + 1, subset);
+    }
+}
