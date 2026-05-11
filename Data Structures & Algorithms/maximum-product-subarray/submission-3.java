37018731836c1a580e8cc5227a45648375f02fae
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-05-11 10:27:47 +0300

    Add: maximum-product-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-3.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int maxProduct(int[] numbers) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> current = new ArrayList<>(); 
+        int result = Integer.MIN_VALUE;
+        int size = numbers.length;
+
+        for (int number : numbers) {
+            result = Math.max(result, number);
+            if (number == 0) {
+                if (!current.isEmpty()) {
+                    list.add(current);
+                }
+                current = new ArrayList<>();
+            } else {
+                current.add(number);
+            }
+        }
+        if (!current.isEmpty()) {
+            list.add(current);
+        }
+
+        for (List<Integer> subList : list) {
+            int negs = 0;
+            for (int item : subList) {
+                if (item < 0) {
+                    negs++;
+                }
+            }
+
+            int prod = 1;
+            int need = (negs % 2 == 0) ? negs : (negs - 1);
+            negs = 0;
+            for (int i = 0, j = 0; i < subList.size(); i++) {
+                prod *= subList.get(i);
+                if (subList.get(i) < 0) {
+                    negs++;
+                    while (negs > need) {
+                        prod /= subList.get(j);
+                        if (subList.get(j) < 0) {
+                            negs--;
+                        }
+                        j++;
+                    }
+                }
+                if (j <= i) {
+                    result = Math.max(result, prod);
+                }
+            }
+        }
+        return result;
+    }
+}
