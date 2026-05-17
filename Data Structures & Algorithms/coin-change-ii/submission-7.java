7f1b6e906ab0e04e3c447a871c0abe0b49bc1d65
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-05-17 19:55:30 +0300

    Add: coin-change-ii - submission-7

---
+++ Data Structures & Algorithms/coin-change-ii/submission-7.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] array = new int[amount + 1];
+        array[0] = 1;
+        for (int coin : coins ) {
+            for (int a = coin; a <= amount; a++) {
+                array[a] = array[a] + array[a - coin];
+            }
+        }
+        return array[amount];
+    }
+}
