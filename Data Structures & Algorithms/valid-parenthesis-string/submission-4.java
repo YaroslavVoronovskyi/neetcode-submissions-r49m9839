796e581c286c8cf8d85a3ccc0d14084dec17dd89
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-05-22 08:57:11 +0300

    Add: valid-parenthesis-string - submission-4

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-4.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean checkValidString(String expression) {
+        int size = expression.length();
+        Stack<Integer> left = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+        for (int index = 0; index < size; index++) {
+            char character = expression.charAt(index);
+            if (character == '(') {
+                left.push(index);
+            } else if (character == '*') {
+                star.push(index);
+            } else {
+                if (left.isEmpty() && star.isEmpty()) {
+                    return false;
+                }
+                if (!left.isEmpty()) {
+                    left.pop();
+                } else {
+                    star.pop();
+                }
+            }
+        
+        }
+        while (!left.isEmpty() && !star.isEmpty()) {
+            if (left.pop() > star.pop()) {
+                return false;
+            }
+        }
+        return left.isEmpty();
+    }
+}
