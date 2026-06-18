662685b8b2ddfc12e812a6d7156200db1a7f48f6
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-06-18 10:35:07 +0300

    Add: burst-balloons - submission-2

---
+++ Data Structures & Algorithms/burst-balloons/submission-2.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxCoins(int[] nums) {
+        int size = nums.length;
+        int[] newNums = new int[size + 2];
+        newNums[0] = newNums[size + 1] = 1;
+        System.arraycopy(nums, 0, newNums, 1, size);
+        int[][] dp = new int[size + 2][size + 2];
+        for (int i = 0; i <= size; i++) {
+            for (int j = 0; j <= size; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return depthFirstSearch(newNums, 1, newNums.length - 2, dp);
+    }
+
+    public int depthFirstSearch(int[] nums, int left, int right, int[][] dp) {
+        if (nums.length == 2) {
+            return 0;
+        }
+        if (left > right) {
+            return 0;
+        }
+        if (dp[left][right] != -1) {
+            return dp[left][right];
+        }
+        dp[left][right] = 0;
+        for (int i = left; i <= right; i++) {
+            int coins = nums[left - 1] * nums[i] * nums[right + 1];
+            coins += depthFirstSearch(nums, left, i - 1, dp) + depthFirstSearch(nums, i + 1, right, dp);
+            dp[left][right] = Math.max(dp[left][right], coins);
+        }
+        return dp[left][right];
+    }
+}
