cb0953ef100f0ac583803cd9c2b7ddaa4bc72b43
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-06-27 09:49:07 +0300

    Add: design-hashset - submission-2

---
+++ Data Structures & Algorithms/design-hashset/submission-2.java
@@ -0,0 +1,61 @@
+class MyHashSet {
+
+    private static class ListNode {
+        int key;
+        ListNode next;
+
+        ListNode(int key) {
+            this.key = key;
+        }
+    }
+
+    private final ListNode[] data;
+
+    public MyHashSet() {
+        data = new ListNode[10000];
+        for (int index = 0; index < data.length; index++) {
+            data[index] = new ListNode(0);
+        }
+    }
+    
+    public void add(int key) {
+        ListNode current = data[key % data.length];
+        while (current.next != null) {
+            if (current.next.key == key) {
+                return;
+            }
+            current = current.next;
+        }
+        current.next = new ListNode(key);
+    }
+    
+    public void remove(int key) {
+        ListNode current = data[key % data.length];
+        while (current.next != null) {
+            if (current.next.key == key) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+    
+    public boolean contains(int key) {
+        ListNode current = data[key % data.length];
+        while (current.next != null) {
+            if (current.next.key == key) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
