f4b92266bfebd5bef0772aaf73d666f5d5391dda
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-07-02 08:59:56 +0300

    Add: search-insert-position - submission-4

---
+++ Data Structures & Algorithms/search-insert-position/submission-4.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int searchInsert(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length;
+        while (left < right) {
+            int middle = left + (right - left) / 2;
+            if (numbers[middle] >= target) {
+                right = middle;;
+            } else {
+                left = middle + 1;
+            }
+        }
+        return left;
+    }
+}
