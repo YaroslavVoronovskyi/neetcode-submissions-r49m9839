4bb1e0acd890be734d3391fd4f1a7ea449f99404
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-07-10 09:33:02 +0300

    Add: greatest-common-divisor-of-strings - submission-3

---
+++ Data Structures & Algorithms/greatest-common-divisor-of-strings/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        int g = gcd(str1.length(), str2.length());
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) != str1.charAt(i % g)) {
+                return "";
+            }
+        }
+        for (int i = 0; i < str2.length(); i++) {
+            if (str2.charAt(i) != str1.charAt(i % g)) {
+                return "";
+            }
+        }
+        return str1.substring(0, g);
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
