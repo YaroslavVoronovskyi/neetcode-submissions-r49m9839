fbe02fbeaa631b3c670ecc6a560ab96481b8e44d
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-07-14 10:55:18 +0300

    Add: sort-an-array - submission-5

---
+++ Data Structures & Algorithms/sort-an-array/submission-5.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] sortArray(int[] numbers) {
+        countingSort(numbers);
+        return numbers;
+    }
+
+    private void countingSort(int[] numbers) {
+        Map<Integer, Integer> count = new HashMap<>();
+        int minValue = numbers[0];
+        int maxValue = numbers[0];
+        for (int index = 0; index < numbers.length; index++) {
+            minValue = Math.min(minValue, numbers[index]);
+            maxValue = Math.max(maxValue, numbers[index]);
+            count.put(numbers[index], count.getOrDefault(numbers[index], 0) + 1);
+        }
+        int index = 0;
+        for (int value = minValue; value <= maxValue; ++value) {
+            while (count.getOrDefault(value, 0) > 0) {
+                numbers[index] = value;
+                index += 1;
+                count.put(value, count.get(value) - 1);
+            }
+        }
+    }
+}
