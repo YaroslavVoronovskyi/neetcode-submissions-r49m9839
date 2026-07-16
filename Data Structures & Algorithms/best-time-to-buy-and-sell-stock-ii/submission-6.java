70ab1a06ea8a9c523c263994c8bf5c7b20f697fa
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-07-16 08:59:18 +0300

    Add: best-time-to-buy-and-sell-stock-ii - submission-6

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-6.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int index = 1; index < prices.length; index++) {
+            if (prices[index] > prices[index - 1]) {
+                profit += (prices[index] - prices[index - 1]);
+            }
+        }
+        return profit;
+    }
+}
