6c351ff9d02ad4f676a1ce11bad1d3111567e246
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-07-22 08:11:23 +0300

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int result = 0;
+        int left = 0;
+        int right = people.length - 1;
+        while (left <= right) {
+            int remain = limit - people[right--];
+            result++;
+            if (left <= right && remain >= people[left]) {
+                left++;
+            }
+        }
+        return result;
+    }
+}
