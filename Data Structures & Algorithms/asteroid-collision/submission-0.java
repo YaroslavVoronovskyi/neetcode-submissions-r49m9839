865e8ef694824392418d251b3ad404c6f9595684
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-07-25 08:48:09 +0300

    Add: asteroid-collision - submission-0

---
+++ Data Structures & Algorithms/asteroid-collision/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int asteroid : asteroids) {
+            while (!stack.isEmpty() && asteroid < 0 && stack.peek() > 0) {
+                int diff = asteroid + stack.peek();
+                if (diff < 0) {
+                    stack.pop();
+                } else if (diff > 0) {
+                    asteroid = 0;
+                } else {
+                    asteroid = 0;
+                    stack.pop();
+                }
+            }
+            if (asteroid != 0) {
+                stack.add(asteroid);
+            }
+        }
+        return stack.stream().mapToInt(index -> index).toArray();
+    }
+}
