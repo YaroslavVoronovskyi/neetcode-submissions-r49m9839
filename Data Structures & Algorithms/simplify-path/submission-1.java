4bdebd246a8597b8f95c4db49f81244f087529e7
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-07-27 08:32:32 +0300

    Add: simplify-path - submission-1

---
+++ Data Structures & Algorithms/simplify-path/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        StringBuilder current = new StringBuilder();
+
+        for (char character : (path + "/").toCharArray()) {
+            if (character == '/') {
+                if (current.toString().equals("..")) {
+                    if (!stack.isEmpty()) {
+                        stack.pop();
+                    }
+                } else if (!current.isEmpty() && !current.toString().equals(".")) {
+                    stack.push(current.toString());
+                }
+                current.setLength(0);
+            } else {
+                current.append(character);
+            }
+        } 
+        return "/" + String.join("/", stack);
+    }
+}
