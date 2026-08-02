dd75bb08d8f9a9ab46e6730d4ad262b44b3aaef6
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-08-02 10:10:32 +0300

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    List<List<Integer>> result;
+
+    public List<List<Integer>> combine(int n, int k) {
+        result = new ArrayList<>();
+        backtrack(1, n, k, new ArrayList<>());
+        return result;
+    }
+
+    private void backtrack(int i, int n, int k, List<Integer> comb) {
+        if (i > n) {
+            if (comb.size() == k) {
+                result.add(new ArrayList<>(comb));
+            }
+            return;
+        }
+        comb.add(i);
+        backtrack(i + 1, n, k, comb);
+        comb.remove(comb.size() - 1);
+        backtrack(i + 1, n, k, comb);
+    }
+}
