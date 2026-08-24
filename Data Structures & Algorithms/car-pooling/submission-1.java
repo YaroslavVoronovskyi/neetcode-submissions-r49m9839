a8cae314b9fb1cdce1f7263e13f40fa77343be56
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-08-24 08:52:53 +0300

    Add: car-pooling - submission-1

---
+++ Data Structures & Algorithms/car-pooling/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        List<int[]> points = new ArrayList<>();
+        for(int[] trip : trips) {
+            int passengers = trip[0];
+            int start = trip[1];
+            int end = trip[2];
+            points.add(new int[]{start, passengers});
+            points.add(new int[]{end, -passengers});
+        }
+        points.sort((a, b) -> a[0] == b[0] ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0]));
+
+        int curPass = 0;
+        for (int[] point : points) {
+            curPass += point[1];
+            if (curPass > capacity) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
