c901daa617d6e682b04d41b1a6d562e04bf815cf
Yaroslav <58781960+YaroslavVoronovskyi@users.noreply.github.com>
2026-09-26 16:57:07 +0300

    Add: count-connected-components - submission-3

---
+++ Data Structures & Algorithms/count-connected-components/submission-3.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        List<List<Integer>> adj = new ArrayList<>();
+        boolean[] visit = new boolean[n];
+        
+        for (int index = 0; index < n; index++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+
+        int result = 0;
+        for (int node = 0; node < n; node++) {
+            if(!visit[node]) {
+                depthFirstSearch(adj, visit, node);
+                result++;
+            } 
+        }
+        return result;
+    }
+
+    private void depthFirstSearch(List<List<Integer>> adj, boolean[] visit, int node) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(node);
+        while (!stack.isEmpty()) {
+            int current = stack.pop();
+            if (visit[current]) {
+                continue;
+            }
+            visit[current] = true;
+            for (int nei : adj.get(current)) {
+                if (!visit[nei]) {
+                    stack.push(nei);
+                }
+            }
+        }
+    }
+}
